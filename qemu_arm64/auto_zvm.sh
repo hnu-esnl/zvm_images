83669a8bf39b74d337e6c1bad5279b8b3383d819
Guoqi Xie <xgqman@hnu.edu.cn>
2025-01-15 11:40:37 +0800

    Image: replace qemu emulator
    We can use default qemu to test zvm,
    rather than replaced qemu.
    
    Signed-off-by: Guoqi Xie <xgqman@hnu.edu.cn>

--- qemu_arm64/auto_zvm.sh
+++ qemu_arm64/auto_zvm.sh
@@ -3,7 +3,7 @@
 OPS=$1
 PLAT=$2
 
-ops_array=("debugserver")
+ops_array=("debugserver" "test")
 plat_array=("qemu_max_smp")
 
 ops_found=false
@@ -33,6 +33,17 @@ do
     fi
 done
 
+# test system
+if [ "$OPS" = "${ops_array[1]}" ]; then
+    qemu-system-aarch64 -cpu max -m 4G -nographic -machine virt,virtualization=on,gic-version=3 \
+    -net none -pidfile qemu.pid -chardev stdio,id=con,mux=on \
+    -serial chardev:con -mon chardev=con,mode=readline -smp cpus=4 \
+    -device loader,file=/home/proctor-a/xiong/zephyrproject/zephyr/build/zephyr/zephyr.bin,addr=0xe0000000,force-raw=on \
+    -device loader,file=./linux/Image_withFS,addr=0xe4000000,force-raw=on \
+    -device loader,file=./linux/linux-qemu-virt.dtb,addr=0xec000000 \
+    -kernel ./zvm_host_test.elf
+fi
+
 if [ "$plat_found" = false ]; then
     echo "Invalid platform. Please use one of the following:"
     for index in "${!plat_array[@]}"; do
@@ -45,13 +56,12 @@ fi
 # debug system
 if [ "$OPS" = "${ops_array[0]}" ]; then
     if [ "$PLAT" = "${plat_array[0]}" ]; then
-        strace -o qemu_bug.txt ./qemu-system-aarch64 \
-        -cpu max -m 4G -nographic -machine virt,virtualization=on,gic-version=3 \
+        qemu-system-aarch64 -cpu max -m 4G -nographic -machine virt,virtualization=on,gic-version=3 \
         -net none -pidfile qemu.pid -chardev stdio,id=con,mux=on \
-        -serial chardev:con -mon chardev=con,mode=readline -serial pty -serial pty -smp cpus=4 \
-        -device loader,file=./zephyr/zephyr.bin,addr=0x60000000,force-raw=on \
-        -device loader,file=./linux/Image_withFS,addr=0x70000000,force-raw=on \
-        -device loader,file=./linux/linux-qemu-virt.dtb,addr=0x75000000 \
+        -serial chardev:con -mon chardev=con,mode=readline -smp cpus=4 \
+        -device loader,file=/home/proctor-a/xiong/zephyrproject/zephyr/build/zephyr/zephyr.bin,addr=0xe0000000,force-raw=on \
+        -device loader,file=./linux/Image_withFS,addr=0xe4000000,force-raw=on \
+        -device loader,file=./linux/linux-qemu-virt.dtb,addr=0xec000000 \
         -kernel ./zvm_host.elf
 ### using gdb to connect it:
 # gdb-multiarch -q -ex 'file ./build/zephyr/zvm_host.elf' -ex 'target remote localhost:1234'
